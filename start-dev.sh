f0dbbbc36be789426a6c75fe05ff856933d79233
agung-transfashion <agung@transfashionindonesia.com>
2024-12-05 17:12:42 +0700

    update sh script

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cmd="php ./start.php --config=config-development.php"
+# dev-version, menggunakan config-development.php
+# kirim data sales dari TransBrowser ke kalista
+# Agung Nugroho <agung@transfashionindonesia.com>
+# Created at 5 Desember 2024
+
+currdir=$(dirname "$(realpath "$0")")
+cmd="php $currdir/start.php --config=config-development.php"
 
 echo "start sending data..."
 echo $cmd
--- start-prod.sh
+++ start-prod.sh
@@ -4,9 +4,10 @@
 # Agung Nugroho <agung@transfashionindonesia.com>
 # Created at 5 Desember 2024
 
-cmd="php ./start.php --config=config-production.php"
+currdir=$(dirname "$(realpath "$0")")
+cmd="php $currdir/start.php --config=config-production.php"
 
 echo "start sending data..."
 echo $cmd
-$cmd
+#$cmd
 
